c085fcc3dd0313c56c7b5f075ae9bcb419c4adef
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-07-29 12:33:01 +0600

    add borsh dep, setup read fuzzing

--- build.zig
+++ build.zig
@@ -24,16 +24,23 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const borsh = b.dependency("borsh", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
     const deps = Dependencies{
         .filterz = filterz,
         .zstd = zstd,
         .lz4 = lz4,
         .arrow = arrow,
+        .borsh = borsh,
     };
 
     const mod = b.addModule("olive", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
+        .optimize = optimize,
     });
     deps.add_to_module(mod);
 
@@ -49,6 +56,7 @@ pub fn build(b: *std.Build) void {
     // Have to have single fuzz entrypoint until this is solved: https://github.com/ziglang/zig/issues/23738
     // So create one target per fuzz target using this function and run the commands seperately.
     add_fuzz_target(b, deps, "fuzz_roundtrip", "src/fuzz_roundtrip.zig", "run fuzz tests for read/write roundtrip", target, optimize);
+    add_fuzz_target(b, deps, "fuzz_read", "src/fuzz_read.zig", "run fuzz tests for read", target, optimize);
 }
 
 fn add_fuzz_target(b: *std.Build, deps: Dependencies, command_name: []const u8, root_source_file: []const u8, description: []const u8, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void {
@@ -76,11 +84,13 @@ const Dependencies = struct {
     arrow: *std.Build.Dependency,
     lz4: *std.Build.Dependency,
     zstd: *std.Build.Dependency,
+    borsh: *std.Build.Dependency,
 
     fn add_to_module(deps: *const Dependencies, mod: *std.Build.Module) void {
         mod.addImport("filterz", deps.filterz.module("filterz"));
         mod.linkLibrary(deps.zstd.artifact("zstd"));
         mod.linkLibrary(deps.lz4.artifact("lz4"));
         mod.addImport("arrow", deps.arrow.module("arrow"));
+        mod.addImport("borsh", deps.borsh.module("borsh"));
     }
 };
