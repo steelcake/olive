e66576c42ea585c4ba2634e4273f42f2e49bbae0
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-08-24 17:41:07 +0600

    merge all fuzz tests, re-use allocation through all fuzz runs

--- build.zig
+++ build.zig
@@ -29,20 +29,16 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const deps = Dependencies{
-        .filterz = filterz,
-        .zstd = zstd,
-        .lz4 = lz4,
-        .arrow = arrow,
-        .borsh = borsh,
-    };
-
     const mod = b.addModule("olive", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-    deps.add_to_module(mod);
+    mod.addImport("filterz", filterz.module("filterz"));
+    mod.linkLibrary(zstd.artifact("zstd"));
+    mod.linkLibrary(lz4.artifact("lz4"));
+    mod.addImport("arrow", arrow.module("arrow"));
+    mod.addImport("borsh", borsh.module("borsh"));
 
     const mod_tests = b.addTest(.{
         .root_module = mod,
@@ -53,46 +49,21 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
 
-    // Have to have single fuzz entrypoint until this is solved: https://github.com/ziglang/zig/issues/23738
-    // So create one target per fuzz target using this function and run the commands seperately.
-    add_fuzz_target(b, deps, "fuzz_roundtrip", "src/fuzz_roundtrip.zig", "run fuzz tests for read/write roundtrip", target, optimize);
-    add_fuzz_target(b, deps, "fuzz_read", "src/fuzz_read.zig", "run fuzz tests for read", target, optimize);
-    add_fuzz_target(b, deps, "fuzz_header_de", "src/fuzz_header_de.zig", "run fuzz tests for deserializing header", target, optimize);
-    add_fuzz_target(b, deps, "fuzz_schema_de", "src/fuzz_schema_de.zig", "run fuzz tests for deserializing schema", target, optimize);
-}
-
-fn add_fuzz_target(b: *std.Build, deps: Dependencies, command_name: []const u8, root_source_file: []const u8, description: []const u8, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) void {
-    const mod = b.createModule(.{
-        .root_source_file = b.path(root_source_file),
-        .target = target,
-        .optimize = optimize,
-    });
-    deps.add_to_module(mod);
     const fuzz = b.addTest(.{
-        .root_module = mod,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/fuzz.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
         // Required for running fuzz tests
         // https://github.com/ziglang/zig/issues/23423
         .use_llvm = true,
     });
+    fuzz.root_module.addImport("olive", mod);
+    fuzz.root_module.addImport("arrow", arrow.module("arrow"));
 
     const run_fuzz = b.addRunArtifact(fuzz);
 
-    const fuzz_step = b.step(command_name, description);
+    const fuzz_step = b.step("fuzz", "run fuzz tests");
     fuzz_step.dependOn(&run_fuzz.step);
 }
-
-const Dependencies = struct {
-    filterz: *std.Build.Dependency,
-    arrow: *std.Build.Dependency,
-    lz4: *std.Build.Dependency,
-    zstd: *std.Build.Dependency,
-    borsh: *std.Build.Dependency,
-
-    fn add_to_module(deps: *const Dependencies, mod: *std.Build.Module) void {
-        mod.addImport("filterz", deps.filterz.module("filterz"));
-        mod.linkLibrary(deps.zstd.artifact("zstd"));
-        mod.linkLibrary(deps.lz4.artifact("lz4"));
-        mod.addImport("arrow", deps.arrow.module("arrow"));
-        mod.addImport("borsh", deps.borsh.module("borsh"));
-    }
-};
--- src/fuzz.zig
+++ src/fuzz.zig
@@ -1,25 +1,86 @@
 const std = @import("std");
+const FixedBufferAllocator = std.heap.FixedBufferAllocator;
 const Allocator = std.mem.Allocator;
 const ArenaAllocator = std.heap.ArenaAllocator;
-const FixedBufferAllocator = std.heap.FixedBufferAllocator;
 
-const borsh = @import("borsh");
-
+const olive = @import("olive");
 const arrow = @import("arrow");
-const validate = arrow.validate.validate;
 
-const chunk_mod = @import("./chunk.zig");
-const Chunk = chunk_mod.Chunk;
-const write = @import("./write.zig");
-const read = @import("./read.zig");
-const schema_mod = @import("./schema.zig");
-const Schema = schema_mod.Schema;
-const TableSchema = schema_mod.TableSchema;
-const dict_impl = @import("./dict.zig");
-const header_mod = @import("./header.zig");
-const FuzzInput = @import("./fuzz_input.zig").FuzzInput;
+fn fuzz_schema_de(data: []const u8, gpa: Allocator) !void {
+    const de_buf = try gpa.alloc(u8, 1 << 14);
+    defer gpa.free(de_buf);
 
-fn roundtrip_test(input: *FuzzInput, alloc: Allocator) !void {
+    var fb_alloc = std.heap.FixedBufferAllocator.init(de_buf);
+    const alloc = fb_alloc.allocator();
+
+    const sch = olive.schema.Schema.deserialize(data, alloc, 30) catch return;
+    sch.validate() catch return;
+}
+
+fn fuzz_read(data: []const u8, gpa: Allocator) !void {
+    var input = olive.fuzz_input.FuzzInput.init(data);
+
+    const alloc_buf = try gpa.alloc(u8, 1 << 28);
+    defer gpa.free(alloc_buf);
+
+    var fba = std.heap.FixedBufferAllocator.init(alloc_buf);
+    const alloc = fba.allocator();
+
+    const head = try input.make_header(alloc);
+
+    const schema = make_schema: {
+        var scratch_arena = std.heap.ArenaAllocator.init(alloc);
+        defer scratch_arena.deinit();
+        const scratch_alloc = scratch_arena.allocator();
+        break :make_schema try input.make_schema(alloc, scratch_alloc);
+    };
+
+    const out_chunk = read: {
+        var scratch_arena = std.heap.ArenaAllocator.init(alloc);
+        defer scratch_arena.deinit();
+        const scratch_alloc = scratch_arena.allocator();
+
+        var h = head;
+
+        h.data_section_size = @intCast(@min(input.inner.data.len, std.math.maxInt(u32)));
+
+        break :read olive.read.read(.{
+            .alloc = alloc,
+            .header = &h,
+            .schema = &schema,
+            .scratch_alloc = scratch_alloc,
+            .data_section = input.inner.data[0..h.data_section_size],
+        }) catch return;
+    };
+
+    for (out_chunk.tables) |out_table| {
+        for (out_table.fields) |*out_field| {
+            try arrow.validate.validate(out_field);
+        }
+    }
+
+    const out_tables = try out_chunk.to_arrow(alloc);
+
+    for (out_tables) |out_table| {
+        for (out_table) |*out_field| {
+            try arrow.validate.validate(out_field);
+        }
+    }
+}
+
+fn fuzz_header_de(data: []const u8, gpa: Allocator) !void {
+    const de_buf = try gpa.alloc(u8, 1 << 14);
+    defer gpa.free(de_buf);
+
+    var fb_alloc = std.heap.FixedBufferAllocator.init(de_buf);
+    const alloc = fb_alloc.allocator();
+
+    _ = olive.header.Header.deserialize(data, alloc, 30) catch {};
+}
+
+fn fuzz_roundtrip(data: []const u8, alloc: Allocator) !void {
+    var input = olive.fuzz_input.FuzzInput.init(data);
+
     const chunk_buf = try alloc.alloc(u8, 1 << 24);
     defer alloc.free(chunk_buf);
     var chunk_fb_alloc = FixedBufferAllocator.init(chunk_buf);
@@ -51,7 +112,7 @@ fn roundtrip_test(input: *FuzzInput, alloc: Allocator) !void {
         defer scratch_arena.deinit();
         const scratch_alloc = scratch_arena.allocator();
 
-        break :write try write.write(.{
+        break :write try olive.write.write(.{
             .chunk = &chunk,
             .compression = .lz4,
             .header_alloc = header_alloc,
@@ -78,11 +139,11 @@ fn roundtrip_test(input: *FuzzInput, alloc: Allocator) !void {
 
     var out_header_fb_alloc = std.heap.FixedBufferAllocator.init(try out_alloc.alloc(u8, 1 << 19));
     const out_header_alloc = out_header_fb_alloc.allocator();
-    const out_header = try header_mod.Header.deserialize(header_bytes, out_header_alloc, 40);
+    const out_header = try olive.header.Header.deserialize(header_bytes, out_header_alloc, 40);
 
     var out_schema_fb_alloc = std.heap.FixedBufferAllocator.init(try out_alloc.alloc(u8, 1 << 19));
     const out_schema_alloc = out_schema_fb_alloc.allocator();
-    const out_schema = try schema_mod.Schema.deserialize(schema_bytes, out_schema_alloc, 40);
+    const out_schema = try olive.schema.Schema.deserialize(schema_bytes, out_schema_alloc, 40);
     std.debug.assert(chunk.schema.eql(&out_schema));
 
     const out_chunk = read: {
@@ -90,7 +151,7 @@ fn roundtrip_test(input: *FuzzInput, alloc: Allocator) !void {
         defer scratch_arena.deinit();
         const scratch_alloc = scratch_arena.allocator();
 
-        break :read try read.read(.{
+        break :read try olive.read.read(.{
             .data_section = data_section[0..out_header.data_section_size],
             .scratch_alloc = scratch_alloc,
             .schema = chunk.schema,
@@ -104,9 +165,8 @@ fn roundtrip_test(input: *FuzzInput, alloc: Allocator) !void {
         std.debug.assert(out_table.num_rows == chunk_table.num_rows);
 
         for (out_table.fields, chunk_table.fields) |*out_field, *chunk_field| {
-            try validate(out_field);
-            try validate(chunk_field);
-
+            try arrow.validate.validate(out_field);
+            try arrow.validate.validate(chunk_field);
             arrow.equals.equals(out_field, chunk_field);
         }
     }
@@ -119,16 +179,25 @@ fn roundtrip_test(input: *FuzzInput, alloc: Allocator) !void {
         std.debug.assert(out_table.len == table.len);
 
         for (out_table, table) |*out_field, *field| {
-            try validate(out_field);
-            try validate(field);
-
+            try arrow.validate.validate(out_field);
+            try arrow.validate.validate(field);
             arrow.equals.equals(out_field, field);
         }
     }
 }
 
-fn to_fuzz(data: []const u8) !void {
-    var general_purpose_allocator: std.heap.GeneralPurposeAllocator(.{}) = .init;
+const FuzzContext = struct {
+    fb_alloc: *FixedBufferAllocator,
+};
+
+fn run_fuzz_test(comptime fuzz_one: fn (data: []const u8, gpa: Allocator) anyerror!void, data: []const u8, fb_alloc: *FixedBufferAllocator) anyerror!void {
+    fb_alloc.reset();
+
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{
+        .backing_allocator_zeroes = false,
+    }){
+        .backing_allocator = fb_alloc.allocator(),
+    };
     const gpa = general_purpose_allocator.allocator();
     defer {
         switch (general_purpose_allocator.deinit()) {
@@ -139,17 +208,21 @@ fn to_fuzz(data: []const u8) !void {
         }
     }
 
-    var input = FuzzInput.init(data);
-
-    try roundtrip_test(&input, gpa);
-}
-
-fn to_fuzz_wrap(_: void, data: []const u8) anyerror!void {
-    return to_fuzz(data) catch |e| {
+    fuzz_one(data, gpa) catch |e| {
         if (e == error.ShortInput) return {} else return e;
     };
 }
 
+fn to_fuzz_wrap(ctx: FuzzContext, data: []const u8) anyerror!void {
+    try run_fuzz_test(fuzz_roundtrip, data, ctx.fb_alloc);
+    try run_fuzz_test(fuzz_header_de, data, ctx.fb_alloc);
+    try run_fuzz_test(fuzz_read, data, ctx.fb_alloc);
+    try run_fuzz_test(fuzz_schema_de, data, ctx.fb_alloc);
+}
+
 test "fuzz" {
-    try std.testing.fuzz({}, to_fuzz_wrap, .{});
+    var fb_alloc = FixedBufferAllocator.init(std.heap.page_allocator.alloc(u8, 1 << 30) catch unreachable);
+    try std.testing.fuzz(FuzzContext{
+        .fb_alloc = &fb_alloc,
+    }, to_fuzz_wrap, .{});
 }
