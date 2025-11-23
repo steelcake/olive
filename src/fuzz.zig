e59199b15083e8fd5880f43a0eeddff8968f3d2b
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-11-24 02:51:00 +0600

    fuzz_roundtrip

--- src/fuzz.zig
+++ src/fuzz.zig
@@ -60,141 +60,103 @@ test fuzz_read {
     );
 }
 
-// fn fuzz_roundtrip(data: []const u8, alloc: Allocator) !void {
-//     const StaticBufs = struct {
-//         var data_section: ?[]u8 = null;
-//         var header_bytes_buf: ?[]u8 = null;
-//         var schema_bytes_buf: ?[]u8 = null;
+fn fuzz_roundtrip(
+    data_section_buf: []u8,
+    input: *FuzzInput,
+    dbg_alloc: Allocator,
+) fuzzin.Error!void {
+    var arena = ArenaAllocator.init(dbg_alloc);
+    defer arena.deinit();
+    var limited_alloc_v = fuzzin.LimitedAllocator.init(arena.allocator(), 1 << 20);
+    const limited_alloc = limited_alloc_v.allocator();
 
-//         fn get_data_section() []u8 {
-//             if (data_section) |ds| {
-//                 return ds;
-//             } else {
-//                 const ds = std.heap.page_allocator.alloc(u8, 1 << 28) catch unreachable;
-//                 data_section = ds;
-//                 return ds;
-//             }
-//         }
+    const schema = try olive.fuzz_input.schema(input, limited_alloc);
+    const arrow_chunk = try olive.fuzz_input.arrow_chunk(&schema, input, limited_alloc);
 
-//         fn get_header_bytes_buf() []u8 {
-//             if (header_bytes_buf) |b| {
-//                 return b;
-//             } else {
-//                 const b = std.heap.page_allocator.alloc(u8, 1 << 20) catch unreachable;
-//                 header_bytes_buf = b;
-//                 return b;
-//             }
-//         }
+    const alloc = arena.allocator();
 
-//         fn get_schema_bytes_buf() []u8 {
-//             if (schema_bytes_buf) |b| {
-//                 return b;
-//             } else {
-//                 const b = std.heap.page_allocator.alloc(u8, 1 << 20) catch unreachable;
-//                 schema_bytes_buf = b;
-//                 return b;
-//             }
-//         }
-//     };
+    const olive_chunk = make_chunk: {
+        var scratch_arena = std.heap.ArenaAllocator.init(dbg_alloc);
+        defer scratch_arena.deinit();
+        const scratch_alloc = scratch_arena.allocator();
 
-//     var input = olive.fuzz_input.FuzzInput.init(data);
+        break :make_chunk olive.chunk.Chunk.from_arrow(
+            &schema,
+            arrow_chunk,
+            alloc,
+            scratch_alloc,
+        ) catch unreachable;
+    };
 
-//     var chunk_arena = ArenaAllocator.init(alloc);
-//     defer chunk_arena.deinit();
-//     const chunk_alloc = chunk_arena.allocator();
+    // min 1B max 1 GB
+    const page_size = ((try input.int(u32)) % (1 << 30)) + 1;
 
-//     const chunk = make_chunk: {
-//         var scratch_arena = ArenaAllocator.init(alloc);
-//         defer scratch_arena.deinit();
-//         const scratch_alloc = scratch_arena.allocator();
-//         break :make_chunk try input.make_chunk(chunk_alloc, scratch_alloc);
-//     };
+    const compression_bias: olive.write.CompressionBias = if (try input.boolean())
+        .balanced
+    else
+        .read_optimized;
 
-//     var header_arena = ArenaAllocator.init(alloc);
-//     defer header_arena.deinit();
-//     const header_alloc = header_arena.allocator();
+    const header = write: {
+        var scratch_arena = ArenaAllocator.init(alloc);
+        defer scratch_arena.deinit();
+        const scratch_alloc = scratch_arena.allocator();
 
-//     var filter_arena = ArenaAllocator.init(alloc);
-//     defer filter_arena.deinit();
-//     const filter_alloc = filter_arena.allocator();
+        break :write olive.write.write(.{
+            .chunk = &olive_chunk,
+            .header_alloc = alloc,
+            .scratch_alloc = scratch_alloc,
+            .data_section = data_section_buf,
+            .page_size = page_size,
+            .compression_bias = compression_bias,
+        }) catch unreachable;
+    };
 
-//     // min 1KB max 1 GB
-//     const page_size = ((try input.inner.int(u32)) % (1 << 30)) + (1 << 10);
+    const data_section = data_section_buf[0..header.data_section_size];
 
-//     const data_section = StaticBufs.get_data_section();
+    const out_chunk = read: {
+        var scratch_arena = ArenaAllocator.init(alloc);
+        defer scratch_arena.deinit();
+        const scratch_alloc = scratch_arena.allocator();
 
-//     const input_header = write: {
-//         var scratch_arena = ArenaAllocator.init(alloc);
-//         defer scratch_arena.deinit();
-//         const scratch_alloc = scratch_arena.allocator();
+        break :read try olive.read.read(.{
+            .data_section = data_section,
+            .scratch_alloc = scratch_alloc,
+            .schema = &schema,
+            .alloc = alloc,
+            .header = &header,
+        });
+    };
 
-//         break :write try olive.write.write(.{
-//             .chunk = &chunk,
-//             .compression = try input.make_chunk_compression(chunk.schema, chunk_alloc),
-//             .header_alloc = header_alloc,
-//             .filter_alloc = filter_alloc,
-//             .scratch_alloc = scratch_alloc,
-//             .data_section = data_section,
-//             .page_size = page_size,
-//         });
-//     };
+    std.debug.assert(out_chunk.tables.len == olive_chunk.tables.len);
+    for (out_chunk.tables, olive_chunk.tables) |out_table, chunk_table| {
+        for (out_table, chunk_table) |*out_field, *chunk_field| {
+            try arrow.validate.validate_array(out_field);
+            try arrow.validate.validate_array(chunk_field);
+            arrow.equals.equals(out_field, chunk_field);
+        }
+    }
 
-//     const schema_bytes_buf = StaticBufs.get_schema_bytes_buf();
-//     const schema_bytes_len = try chunk.schema.serialize(schema_bytes_buf, 40);
-//     const schema_bytes = schema_bytes_buf[0..schema_bytes_len];
+    const out_tables = try out_chunk.to_arrow(alloc);
+    const olive_tables = try olive_chunk.to_arrow(alloc);
 
-//     const header_bytes_buf = StaticBufs.get_header_bytes_buf();
-//     const header_bytes_len = try input_header.serialize(header_bytes_buf, 40);
-//     const header_bytes = header_bytes_buf[0..header_bytes_len];
+    std.debug.assert(out_tables.len == olive_tables.len);
+    for (out_tables, olive_tables) |out_table, table| {
+        std.debug.assert(out_table.len == table.len);
 
-//     var out_arena = ArenaAllocator.init(alloc);
-//     defer out_arena.deinit();
-//     const out_alloc = out_arena.allocator();
+        for (out_table, table) |*out_field, *field| {
+            try arrow.validate.validate_array(out_field);
+            try arrow.validate.validate_array(field);
+            arrow.equals.equals(out_field, field);
+        }
+    }
+}
 
-//     const out_header = try olive.header.Header.deserialize(header_bytes, out_alloc, 40);
-//     const out_schema = try olive.schema.Schema.deserialize(schema_bytes, out_alloc, 40);
-//     std.debug.assert(chunk.schema.eql(&out_schema));
-
-//     const out_chunk = read: {
-//         var scratch_arena = ArenaAllocator.init(alloc);
-//         defer scratch_arena.deinit();
-//         const scratch_alloc = scratch_arena.allocator();
-
-//         break :read try olive.read.read(.{
-//             .data_section = data_section[0..out_header.data_section_size],
-//             .scratch_alloc = scratch_alloc,
-//             .schema = chunk.schema,
-//             .alloc = out_alloc,
-//             .header = &out_header,
-//         });
-//     };
-
-//     std.debug.assert(out_chunk.tables.len == chunk.tables.len);
-//     for (out_chunk.tables, chunk.tables) |out_table, chunk_table| {
-//         std.debug.assert(out_table.num_rows == chunk_table.num_rows);
-
-//         for (out_table.fields, chunk_table.fields) |*out_field, *chunk_field| {
-//             try arrow.validate.validate(out_field);
-//             try arrow.validate.validate(chunk_field);
-//             arrow.equals.equals(out_field, chunk_field);
-//         }
-//     }
-
-//     const out_tables = try out_chunk.to_arrow(out_alloc);
-//     const tables = try chunk.to_arrow(out_alloc);
-
-//     std.debug.assert(out_tables.len == tables.len);
-//     for (out_tables, tables) |out_table, table| {
-//         std.debug.assert(out_table.len == table.len);
-
-//         for (out_table, table) |*out_field, *field| {
-//             try arrow.validate.validate(out_field);
-//             try arrow.validate.validate(field);
-//             arrow.equals.equals(out_field, field);
-//         }
-//     }
-// }
-
-// test "fuzz roundtrip" {
-//     try FuzzWrap(fuzz_roundtrip, 1 << 25).run();
-// }
+test fuzz_roundtrip {
+    const data_section_buf = try std.heap.page_allocator.alloc(u8, 1 << 26);
+    fuzzin.fuzz_test(
+        []u8,
+        data_section_buf,
+        fuzz_roundtrip,
+        0,
+    );
+}
